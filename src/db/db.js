fe252f4133ad0c311111ce3d05364e3bb168994f
ShuvrotoKumar <sdks6409@gmail.com>
2026-03-02 05:20:45 +0600

    database created and connected

--- src/db/db.js
+++ src/db/db.js
@@ -1,2 +1,10 @@
 const mongoose = require("mongoose");
 
+
+async function connectDB(){
+    await mongoose.connect("mongodb+srv://service:service@service.tdc3sak.mongodb.net/?appName=service/service")
+
+    console.log("MongoDB connected");
+}
+
+module.exports = connectDB;
