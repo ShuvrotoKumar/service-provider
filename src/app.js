ffea39db7aaadb234841ba0adcc2b177d32b395e
ShuvrotoKumar <sdks6409@gmail.com>
2026-03-02 05:43:30 +0600

    image buffer completed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
-
+const multer = require("multer");
 const app = express();
 
 // app.listen(3000, () => {
 //     console.log("Server is running on port 3000");
 // });
 
+
+app.use(express.json());
+
+const upload=multer({strorage:multer.diskStorage({})})
+
+app.post("/create-post", upload.single("image"), async (req, res) => {
+    // 
+    console.log(req.body);
+    console.log(req.file);
+});
 module.exports = app;
